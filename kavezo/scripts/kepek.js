2e3f53731a6b04a423dbe49a9b9eea9c5acb987c
Sz4kacs <68946179+Szakacs241@users.noreply.github.com>
2024-10-03 18:28:12 +0200

    Add files via upload

--- kavezo/scripts/kepek.js
+++ kavezo/scripts/kepek.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const galleryImages = document.querySelectorAll(".gallery img");
+// document.addEventListener("DOMContentLoaded", function() {
+//     const galleryImages = document.querySelectorAll(".gallery img");
 
-    galleryImages.forEach(img => {
-        img.addEventListener("click", () => {
-            img.classList.toggle("enlarge");
-        });
-    });
-});
+//     galleryImages.forEach(img => {
+//         img.addEventListener("click", () => {
+//             img.classList.toggle("enlarge");
+//         });
+//     });
+// });
